8af9afe067b98dfb54b5243126211f4fbf511191
robin-UI <robinbiju1901@gmail.com>
2024-11-07 01:09:58 +0530

    landing page

--- src/pages/Home/Components/LandingSection.jsx
+++ src/pages/Home/Components/LandingSection.jsx
@@ -1,28 +1,36 @@
+import ButtonStar  from "../../../assets/images/buttonStar.svg";
+import GrowIcon  from "../../../assets/images/GrowIcon.svg";
+import OnecontinuousLine  from "../../../assets/images/OnecontinuousLine.png";
+
+import { LandingLine, LandingText } from "../../../components/Icons/Icons";
+
 function LandingSection() {
   return (
-    <section className="px-28 h-[840px]">
-      <div className="flex items-center w-full   ">
+    <section className="px-28 h-[720px] relative">
+      <div className="flex items-center w-full">
         <div className="w-1/2">
-          <p className="text-xl font-medium">Begin your journey</p> 
+        <div className="flex items-center gap-2"> <img className="h-[18px]" src={GrowIcon} alt="" /> <LandingText /> <LandingLine/> </div>
+          {/* <p className="text-xl font-medium gradient-text">Begin your journey</p>  */}
           {/* <hr /> */}
-          <h3 className="text-6xl">Launch Your </h3>
-          <h3 className="text-6xl font-bold">Dream Company</h3>
-          <h3 className="text-6xl">in Dubai</h3>
-          <p className="text-lg">
-            Seamless, Fast, and Tailored for Global Entrepreneurs. Globex offers
-            all the services, tools, and resources you need to succeed worldwide
+          <h3 className="text-6xl leading-[66px] font-light">Launch Your </h3>
+          <h3 className="text-6xl leading-[66px] font-bold">Dream Company</h3>
+          <h3 className="text-6xl leading-[66px] font-light mb-5">in Dubai</h3>
+          <p className="text-lg text-gray-500 leading-7 mb-5">
+            Seamless, Fast, and Tailored for Global <br /> Entrepreneurs. Globex offers
+            all the services, tools,<br /> and resources you need to succeed worldwide <br />
             effortlessly and efficiently.
           </p>
-          <button>Excellent 4.7 out of 5 Trustpilot</button>
+          <button className="gradient-border px-4 py-2 rounded-xl text-[#126CD7] font-medium flex gap-2 items-center">  Excellent 4.7 <span className="font-light">out of</span> 5 <img src={ButtonStar} /> Trustpilot</button>
         </div>
 
-        <div className="relative w-1/2 h-full pl-2">
+        <div className="relative w-1/2 pl-2 h-[650px]">
           <img className="absolute top-[10px] left-[50px] w-[215px] h-[255px]" src="../images/homepage/banner-img-1.png" alt="brand-1" />
-          <img className="absolute top-[50px] left-[283px] w-[297px] h-[254px] rounded-3xl" src="../images/homepage/banner-img-2.png" alt="brand-2" />
-          <img className="absolute top-[292px] left-[-10px] w-[297px] h-[243px]" src="../images/homepage/banner-img-4.png" alt="brand-3" />
-          <img className="absolute top-[258px] left-[68px] w-[215px] h-[255px]" src="../images/homepage/banner-img-3.png" alt="brand-4" />
+          <img className="absolute top-[50px] left-[283px] w-[297px] h-[254px] rounded-[30px]" src="../images/homepage/banner-img-3.png" alt="brand-2" />
+          <img className="absolute top-[292px] left-[-35px] w-[297px] h-[243px] rounded-[30px]" src="../images/homepage/banner-img-2.png" alt="brand-3" />
+          <img className="absolute top-[317px] left-[283px] w-[215px] h-[255px]" src="../images/homepage/banner-img-4.png" alt="brand-4" />
         </div>
       </div>
+      <img className="absolute w-screen bottom-[-298px] left-0" src={OnecontinuousLine} alt="" />
     </section>
   );
 }
