1827bc2db9b417c2a8fdfa8263958d3d6a712df1
AlbertSimonA <106898361+AlbertSimonA@users.noreply.github.com>
2024-11-06 21:03:42 +0530

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(270deg, #335BE1 -16.88%, #03E7A3 178.81%)",
+      },
+    },
   },
   plugins: [],
-}
+};
