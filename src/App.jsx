d391a0f7b616be8aee6d6e2be41b8dd7879975b6
robin-UI <robinbiju1901@gmail.com>
2024-11-09 22:49:57 +0530

    app and side

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,7 @@ import { Outlet } from "react-router";
 import Header from "./components/header/Header";
 import Routers from "./router/Router";
 import Footer from "./components/footer/footer"
-<<<<<<< HEAD
 import SideMedia from "./components/sideMedia/SideMedia";
-=======
-import SideMedia from "./components/sideMedia/sideMedia";
->>>>>>> refs/remotes/origin/robin
-
 
 // This is our layout
 
--- src/components/sideMedia/SideMedia.jsx
+++ src/components/sideMedia/SideMedia.jsx
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-
-const SideMedia = () => {
-  return (
-    <div className="side-container bg-slate-400 rounded-[10px_0px_0px_10px] w-[79px] right-0 fixed z-50">
-        <div className="media-icons p-2 pl-3 flex flex-col gap-2">
-          <div className='hover:bg-black' >
-            <img className='h-[40px] w-[40px]' src="../images/homepage/maincall.png" alt="" />
-            <p className='text-white text-center'>Call</p>
-            </div>
-            <div >
-            <img className='h-[40px] w-[40px]' src="../images/homepage/whatsappside.png" alt="" />
-            <p className='text-white'>Whatsapp</p>
-            </div>
-            <div >
-            <img className='h-[40px] w-[40px]' src="../images/homepage/letstalk.png" alt="" />
-            <p className='text-white'>Let's talk</p></div>
-            <div >
-            <img className='h-[40px] w-[40px]' src="../images/homepage/mailus.png" alt="" />
-            <p className='text-white'>Mail us</p>
-            </div>
-
-        </div>
-    </div>
-  )
-}
-
-export default SideMedia
-=======
-import React from "react";
 
 const SideMedia = () => {
   return (
@@ -75,4 +44,3 @@ const SideMedia = () => {
 };
 
 export default SideMedia;
->>>>>>> refs/remotes/origin/robin
